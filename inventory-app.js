854c0166cf0cba57086c76572453635f4d30f284
B-got-banned <onyealilachib0850@student.babcock.edu.ng>
2025-12-22 22:47:59 +0100

    Completed tasks 6, 7 & 8

--- inventory-app.js
+++ inventory-app.js
@@ -1,6 +1,7 @@
+require("dotenv").config()
 const express = require('express');
 const app = express();
-
+const PORT = process.env.PORT || 3000
 app.use(express.json()); // Parse JSON request bodies
 
 // In-memory 'database' (array of objects)
@@ -10,8 +11,41 @@ let products = [
 ];
 
 // Helper function to find a product by ID
-const findProductById = (id) => products.find(p => p.id === parseInt(id));
+const findProductById = (id) => products.find(p => p.id === Number.parseInt(id));
 
 // ROUTES 
 // These are the API interaction points that you guys will complete the logic for.
 // Continue with CRUD operations for the inventory management system.
+
+//Route to list the items out of stock
+app.get("/products/nostock", (req, res) => {
+    const noStock = products.filter(p => p.stock === 0)
+    res.status(200).json(noStock)
+})
+
+//Route to add a new item and stock number
+app.post("/products", (req, res) => {
+    const newItem = {id: products.length + 1, item: req.body.item, stock: req.body.stock}
+    if(!req.body.item || !req.body.stock || req.body.stock < 0){
+        return res.status(400).json({error: "Please make sure that both item and stock are present"})
+    }
+    products.push(newItem)
+    res.status(201).send("New item added successfully!")
+})
+
+//Route to edit the number of items in stock
+app.patch("/products/:id", (req, res) => {
+    const product = products.find(p => p.id === Number.parseInt(req.params.id))
+    if(!product){
+        return res.status(404).json({error: "Product does not exist"})
+    }
+    if(Number.parseInt(req.body.stock) >= 0){
+        product.stock = req.body.stock
+        return res.status(200).json(product)
+    }
+    return res.status(400).json({error: "Please enter valid values for the stock"})
+})
+
+app.listen(PORT, () => {
+    console.log(`App is running on http://localhost:${PORT}`)
+})
