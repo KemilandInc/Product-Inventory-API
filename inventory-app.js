907e1796d7bc04232d142233cc2e95431ef4068d
Denike99 <ojedokunadenike89@gmail.com>
2025-12-23 16:40:46 +0100

    abayomi part

--- inventory-app.js
+++ inventory-app.js
@@ -15,3 +15,21 @@ const findProductById = (id) => products.find((p) => p.id === parseInt(id));
 // ROUTES
 // These are the API interaction points that you guys will complete the logic for.
 // Continue with CRUD operations for the inventory management system.
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/products/:id", (req, res) => {
+  // const id = Number(req.params.id); Converted to number which is parsedInt below
+  // const product = products.find((i) => i.id === id); finding product by id
+
+  // Using helper function to find product by ID
+  const product = findProductById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({ message: "Product not found" });
+  }
+
+  res.json(product);
+});
+
