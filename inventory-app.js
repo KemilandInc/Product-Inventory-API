3f9c1cc1e457619bea0a8e73005e42b2c75f84cf
Kamal El-Mahmoud <elmahmoudkamal0@gmail.com>
2025-12-23 22:43:03 +0000

    Resolved merge conflicts

--- inventory-app.js
+++ inventory-app.js
@@ -45,6 +45,35 @@ app.get("/products/:id", (req, res) => {
   res.json(product);
 });
 
+//Route to list the items out of stock (Task 6)
+app.get("/products/nostock", (req, res) => {
+    const noStock = products.filter(p => p.stock === 0)
+    res.status(200).json(noStock)
+})
+
+//Route to add a new item and stock number (Task 7)
+app.post("/products", (req, res) => {
+    const newItem = {id: products.length + 1, item: req.body.item, stock: req.body.stock}
+    if(!req.body.item || !req.body.stock || req.body.stock < 0){
+        return res.status(400).json({error: "Please make sure that both item and stock are present"})
+    }
+    products.push(newItem)
+    res.status(201).send("New item added successfully!")
+})
+
+//Route to edit the number of items in stock (Task 7)
+app.patch("/products/:id", (req, res) => {
+    const product = products.find(p => p.id === Number.parseInt(req.params.id))
+    if(!product){
+        return res.status(404).json({error: "Product does not exist"})
+    }
+    if(Number.parseInt(req.body.stock) >= 0){
+        product.stock = req.body.stock
+        return res.status(200).json(product)
+    }
+    return res.status(400).json({error: "Please enter valid values for the stock"})
+})
+
 app.listen(PORT, () => {
     console.log(`App is running on http://localhost:${PORT}`)
 })
