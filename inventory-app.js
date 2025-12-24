e03f181ba23a62368687e791fbe63f843a4d95e2
B-got-banned <onyealilachib0850@student.babcock.edu.ng>
2025-12-24 21:52:34 +0100

    Changed position of a route

--- inventory-app.js
+++ inventory-app.js
@@ -30,6 +30,12 @@ app.get("/products/instock", (req, res) => {
   res.json(inStockProducts);
 });
 
+//Route to list the items out of stock
+app.get("/products/nostock", (req, res) => {
+    const noStock = products.filter(p => p.stock === 0)
+    res.status(200).json(noStock)
+})
+
 // (Task 4)
 app.get("/products/:id", (req, res) => {
   // const id = Number(req.params.id); Converted to number which is parsedInt below
@@ -45,11 +51,6 @@ app.get("/products/:id", (req, res) => {
   res.json(product);
 });
 
-//Route to list the items out of stock
-app.get("/products/nostock", (req, res) => {
-    const noStock = products.filter(p => p.stock === 0)
-    res.status(200).json(noStock)
-})
 
 //Route to add a new item and stock number
 app.post("/products", (req, res) => {
